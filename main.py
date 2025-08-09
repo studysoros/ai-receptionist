a3b57df37abf1c769430cbfa9632c0d378585e96
fatsummercat <homelnw77@gmail.com>
2025-08-09 15:24:21 +0700

    twilio receiver

--- main.py
+++ main.py
@@ -40,8 +40,35 @@ async def sts_receiver(sts_ws, twilio_ws, streamsid_queue):
     pass
 
 
-async def twilio_receiver(twilio_ws, sts_ws, streamsid_queue):
-    pass
+async def twilio_receiver(twilio_ws, audio_queue, streamsid_queue):
+    BUFFER_SIZE = 20 * 160
+    inbuffer = bytearray(b"")
+
+    async for message in twilio_ws:
+        try:
+            data = json.load(message)
+            event = data["event"]
+            if event == "start":
+                print("get our streamsid")
+                start = data["start"]
+                streamsid = start["streamSid"]
+                streamsid_queue.put_nowait(streamsid)
+            elif event == "connected":
+                continue
+            elif event == "media":
+                media = data["media"]
+                chunk = base64.b64decode(media["payload"])
+                if media.track == "inbound":
+                    inbuffer.extend(chunk)
+            elif event == "stop":
+                break
+
+            while len(inbuffer) >= BUFFER_SIZE:
+                chunk = inbuffer[:BUFFER_SIZE]
+                audio_queue.put_nowait(chunk)
+                inbuffer = inbuffer[BUFFER_SIZE:]
+        except Exception:
+            break
 
 
 async def twilio_handler(twilio_ws):
